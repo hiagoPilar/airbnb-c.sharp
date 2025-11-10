a4ad3400fbdbf2fb716d81d6c57102d81cc485b8
hiagoPilar <hiagopilar@icloud.com>
2025-11-10 20:46:34 +0000

    Adicionar arquivos de projeto.

---
+++ Infrastructure/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace airbnb_c_.Infrastructure.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace airbnb_c_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
