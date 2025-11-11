38aa976f0c35718be2a642c3ee8183f33a0b335d
hiagoPilar <hiagopilar@icloud.com>
2025-11-11 20:58:02 +0000

    DTOs e AutoMapper

---
+++ WebAPI/DTOs/User/CreateUserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPI.DTOs.User
+{
+    public class CreateUserDto
+    {
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string? PhoneNumber { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+        public string? Bio { get; set; }
+        public string? Language { get; set; }
+    }
+}
---
+++ WebAPI/DTOs/User/UpdateUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.DTOs.User
+{
+    public class UpdateUserDto
+    {
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+        public string? Bio { get; set; }
+        public string? Language { get; set; }
+    }
+}
